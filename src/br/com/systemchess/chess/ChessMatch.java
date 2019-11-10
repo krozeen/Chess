366cd1c5ddcf1e477764f91191bd5e728e995f85
Joao Freitas <joaovotk@gmail.com>
2019-11-10 03:21:05 -0300

    Configuração de exibição das peças no promt

--- src/br/com/systemchess/chess/ChessMatch.java
+++ src/br/com/systemchess/chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package br.com.systemchess.chess;
 
 import br.com.systemchess.boardgame.Board;
+import br.com.systemchess.chess.pieces.King;
+import br.com.systemchess.chess.pieces.Rook;
 
 public class ChessMatch {
 
@@ -26,6 +28,18 @@ public class ChessMatch {
     }
 
     private void initialSetup(){
+        placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
 
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
     }
 }
